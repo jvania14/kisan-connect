7dab26be91695de7fd4400d922e02885956601d7
jvania14 <jainvania14@gmail.com>
2026-08-21 16:15:43 +0530

    remove prebuilt output, force Vercel to build

--- .vercel/output/functions/__server.func/_ssr/tractor-BxFqVpAI.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/tractor-BxFqVpAI.js
-var tractor_default = "/assets/tractor-BanLqRNy.jpg";
-//#endregion
-export { tractor_default as t };
--- .vercel/output/static/assets/tractor-BHronEB4.js
+++
@@ -1,1 +0,0 @@
-var e=`/assets/tractor-BanLqRNy.jpg`;export{e as t};
